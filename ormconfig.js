ebc26597019c85f6455620bc8143723e3a10e0af
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-03-25 18:59:46 +0200

    Enable SQL logging

--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,7 @@ module.exports = {
     url: process.env.DATABASE_URL,
     entities: ["dist/**/*.entity.js"],
     ssl: true,
+    logging: true,
     migrations: ["migrations/dist/*.js"],
     cli: {
         migrationsDir: "migrations"
